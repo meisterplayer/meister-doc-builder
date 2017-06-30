35aee9bb44cebaab518a360e25bf3143a627e20b
Ivo Toby <i.toby@wearetriple.com>
2017-06-30 17:30:00 +0200

    feature(doc-generator): add option for generating CHANGELOGs

--- constants.js
+++ constants.js
@@ -1,10 +1,12 @@
 module.exports = {
     MEISTER_CORE_MODULE_NAME: 'meisterplayer',
     MEISTER_CORE_DOC_NAME: 'Meister',
-    MEISTER_DOCS_DESTINATION: './meister-docs',
+    MEISTER_README_DESTINATION: './meister-docs',
+    MEISTER_CHANGELOG_DESTINATION: './meister-changelogs',
     MEISTER_PATHS: [
         { path: './node_modules/@npm-wearetriple', prefix: 'meister-plugin-' },
         { path: './node_modules/@meisterplayer', prefix: 'plugin-' },
     ],
     README_FILE_NAME: 'README.md',
+    CHANGELOG_FILE_NAME: 'CHANGELOG.md',
 };
